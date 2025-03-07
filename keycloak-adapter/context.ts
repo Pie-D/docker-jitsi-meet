83bd9c98d3fb40895cdbd6cb0dd31fa0369853e4
Diep <trinhquyendiep2002@gmail.com>
2025-03-07 16:22:47 +0700

    update role from active_tenant

--- keycloak-adapter/context.ts
+++ keycloak-adapter/context.ts
@@ -7,34 +7,14 @@
 
 export function createContext(userInfo: Record<string, unknown>) {
   // const realm_access = userInfo.realm_access as { roles: string[] }
-  // const active_tenant = userInfo.active_tenant as {tenant_id: string, tenant_name: string, roles: string[]}
+  const active_tenant = userInfo.active_tenant as {tenant_id: string, tenant_name: string, roles: string[]}
 
-  // const conditions = ["tenant-superadmin", "tenant-admin"]
+  const conditions = ["tenant-superadmin", "tenant-admin"]
   
-  // const isOwner = Array.isArray(active_tenant.roles) 
-  // ? active_tenant.roles.some(role => conditions.includes(role)) 
-  // : false;
+  const isOwner = Array.isArray(active_tenant.roles) 
+  ? active_tenant.roles.some(role => conditions.includes(role)) 
+  : false;
 
-
-  // const context = {
-  //   user: {
-  //     id: userInfo.sub,
-  //     name: userInfo.preferred_username || "",
-  //     email: userInfo.email || "",
-  //     lobby_bypass: true,
-  //     avatar: userInfo.email ? `files.cmcati.vn/ftp/${userInfo.email}` : "",
-  //     security_bypass: true,
-  //     affiliation: isOwner ? "owner" : "member"
-  //   },
-  //   features: {
-  //     livestreaming: true,
-  //     transcription: true,
-  //     recording: isOwner ? true : false
-  //   },
-  //   active_tenant: active_tenant
-  // };
-  const realm_access = userInfo.realm_access as { roles: string[] }
-  const conditions = ["ADMIN", "SUPER_ADMIN"]
 
   const context = {
     user: {
@@ -42,15 +22,16 @@ export function createContext(userInfo: Record<string, unknown>) {
       name: userInfo.preferred_username || "",
       email: userInfo.email || "",
       lobby_bypass: true,
+      avatar: userInfo.email ? `files.cmcati.vn/ftp/${userInfo.email}` : "",
       security_bypass: true,
-      affiliation: realm_access.roles.some(role => conditions.includes(role)) ? "owner" : "member"
+      affiliation: isOwner ? "owner" : "member"
     },
     features: {
       livestreaming: true,
       transcription: true,
-      recording: realm_access.roles.some(role => conditions.includes(role)) ? true : false
+      recording: isOwner ? true : false
     },
-    userInfo : userInfo
+    active_tenant: active_tenant
   };
 
   return context;
